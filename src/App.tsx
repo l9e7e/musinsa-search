5c1ce8bc7888dc2509b0a5ab4e38390e878ffbcb
l9e7e <ehdgur920907@naver.com>
2023-05-05 13:34:45 +0900

    Remove space and Add search input

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
       </div>
       <div className='flex items-center justify-center h-[55px] gap-[5px] mx-[7px]'>
         <button className='flex items-center h-[35px] px-[15px] border border-solid rounded-[18px] border-[#EEEEEE]'>
-          검색{' '}
+          검색
           <img
             className='ml-[6px] !h-[15px] !w-[15px]'
             src='/logo_search.png'
@@ -21,6 +21,15 @@ function App() {
         <button className='h-[35px] px-[15px] border border-solid rounded-[18px] border-[#EEEEEE]'>
           품절포함
         </button>
+      </div>
+      <div className='h-[80px] bg-[#F9F9F9] flex items-center'>
+        <div className='h-[40px] bg-[#FFFFFF] flex items-center mx-[10px] w-full border border-solid border-[#CCCCCC]'>
+          <img
+            className='ml-[10px] mr-[4px] !h-[19px] !w-[19px]'
+            src='/logo_search.png'
+          />
+          <input className='outline-none' placeholder='상품명 검색' />
+        </div>
       </div>
     </div>
   );
