656c048e1833887c0eb4dbcd47c41aad06bf66fe
l9e7e <ehdgur920907@naver.com>
2023-05-05 15:43:07 +0900

    Add sticky header

--- src/App.tsx
+++ src/App.tsx
@@ -53,75 +53,77 @@ function App() {
 
   return (
     <div className='w-[375px] mx-auto'>
-      <div className='flex justify-center items-center h-[50px]'>
-        <img className='!h-[16px]' src='/logo_musinsa.svg' />
-      </div>
-      <div className='flex items-center justify-center h-[55px] gap-[5px] mx-[7px]'>
-        <button className='flex items-center h-[35px] px-[15px] border border-solid rounded-[18px] border-[#EEEEEE]'>
-          <span className='text-[14px] font-normal leading-[21px]'>검색</span>
-          <img
-            className='ml-[6px] !h-[15px] !w-[15px]'
-            src='/logo_search.png'
-          />
-        </button>
-        <button className='h-[35px] px-[15px] border border-solid rounded-[18px] border-[#EEEEEE]'>
-          <span className='text-[14px] font-normal leading-[21px]'>
-            세일상품
-          </span>
-        </button>
-        <button className='h-[35px] px-[15px] border border-solid rounded-[18px] border-[#EEEEEE]'>
-          <span className='text-[14px] font-normal leading-[21px]'>
-            단독상품
-          </span>
-        </button>
-        <button className='h-[35px] px-[15px] border border-solid rounded-[18px] border-[#EEEEEE]'>
-          <span className='text-[14px] font-normal leading-[21px]'>
-            품절포함
-          </span>
-        </button>
-      </div>
-      <div className='h-[50px] flex items-center justify-between ml-[15px]'>
-        <div className='flex gap-[5px]'>
-          <button className='flex items-center h-[26px] px-[10px] rounded-[4px] bg-[#0078FF] text-[#FFFFFF]'>
-            <span className='text-[12px] font-normal leading-[18px]'>
-              스니커즈
-            </span>
+      <div className='sticky top-0 z-10 bg-white'>
+        <div className='flex justify-center items-center h-[50px]'>
+          <img className='!h-[16px]' src='/logo_musinsa.svg' />
+        </div>
+        <div className='flex items-center justify-center h-[55px] gap-[5px] mx-[7px]'>
+          <button className='flex items-center h-[35px] px-[15px] border border-solid rounded-[18px] border-[#EEEEEE]'>
+            <span className='text-[14px] font-normal leading-[21px]'>검색</span>
             <img
               className='ml-[6px] !h-[15px] !w-[15px]'
               src='/logo_search.png'
             />
           </button>
-          <button className='flex items-center h-[26px] px-[10px] rounded-[4px] bg-[#0078FF] text-[#FFFFFF]'>
-            <span className='text-[12px] font-normal leading-[18px]'>
+          <button className='h-[35px] px-[15px] border border-solid rounded-[18px] border-[#EEEEEE]'>
+            <span className='text-[14px] font-normal leading-[21px]'>
               세일상품
             </span>
-            <img
-              className='ml-[6px] !h-[15px] !w-[15px]'
-              src='/logo_search.png'
-            />
           </button>
-          <button className='flex items-center h-[26px] px-[10px] rounded-[4px] bg-[#0078FF] text-[#FFFFFF]'>
-            <span className='text-[12px] font-normal leading-[18px]'>
+          <button className='h-[35px] px-[15px] border border-solid rounded-[18px] border-[#EEEEEE]'>
+            <span className='text-[14px] font-normal leading-[21px]'>
               단독상품
             </span>
-            <img
-              className='ml-[6px] !h-[15px] !w-[15px]'
-              src='/logo_search.png'
-            />
+          </button>
+          <button className='h-[35px] px-[15px] border border-solid rounded-[18px] border-[#EEEEEE]'>
+            <span className='text-[14px] font-normal leading-[21px]'>
+              품절포함
+            </span>
           </button>
         </div>
-        <div className='w-[50px] h-[50px] bg-[#000000]' />
-      </div>
-      <div className='h-[80px] bg-[#F9F9F9] flex items-center'>
-        <div className='h-[40px] bg-[#FFFFFF] flex items-center mx-[10px] w-full border border-solid border-[#CCCCCC]'>
-          <img
-            className='ml-[10px] mr-[4px] !h-[19px] !w-[19px]'
-            src='/logo_search.png'
-          />
-          <input
-            className='outline-none text-[16px] font-normal leading-[24px] placeholder:text-[#AAAAAA]'
-            placeholder='상품명 검색'
-          />
+        <div className='h-[50px] flex items-center justify-between ml-[15px]'>
+          <div className='flex gap-[5px]'>
+            <button className='flex items-center h-[26px] px-[10px] rounded-[4px] bg-[#0078FF] text-[#FFFFFF]'>
+              <span className='text-[12px] font-normal leading-[18px]'>
+                스니커즈
+              </span>
+              <img
+                className='ml-[6px] !h-[15px] !w-[15px]'
+                src='/logo_search.png'
+              />
+            </button>
+            <button className='flex items-center h-[26px] px-[10px] rounded-[4px] bg-[#0078FF] text-[#FFFFFF]'>
+              <span className='text-[12px] font-normal leading-[18px]'>
+                세일상품
+              </span>
+              <img
+                className='ml-[6px] !h-[15px] !w-[15px]'
+                src='/logo_search.png'
+              />
+            </button>
+            <button className='flex items-center h-[26px] px-[10px] rounded-[4px] bg-[#0078FF] text-[#FFFFFF]'>
+              <span className='text-[12px] font-normal leading-[18px]'>
+                단독상품
+              </span>
+              <img
+                className='ml-[6px] !h-[15px] !w-[15px]'
+                src='/logo_search.png'
+              />
+            </button>
+          </div>
+          <div className='w-[50px] h-[50px] bg-[#000000]' />
+        </div>
+        <div className='h-[80px] bg-[#F9F9F9] flex items-center'>
+          <div className='h-[40px] bg-[#FFFFFF] flex items-center mx-[10px] w-full border border-solid border-[#CCCCCC]'>
+            <img
+              className='ml-[10px] mr-[4px] !h-[19px] !w-[19px]'
+              src='/logo_search.png'
+            />
+            <input
+              className='outline-none text-[16px] font-normal leading-[24px] placeholder:text-[#AAAAAA]'
+              placeholder='상품명 검색'
+            />
+          </div>
         </div>
       </div>
       <div className='flex flex-wrap'>
