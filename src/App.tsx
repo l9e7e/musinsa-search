e976ef72d7846d67f22368210c1bcf2f7e8908d2
l9e7e <ehdgur920907@naver.com>
2023-05-05 13:24:18 +0900

    Add Button and image

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,24 @@ function App() {
       <div className='flex justify-center items-center h-[50px]'>
         <img className='!h-[16px]' src='/logo_musinsa.svg' />
       </div>
+      <div className='flex items-center justify-center h-[55px] gap-[5px] mx-[7px]'>
+        <button className='flex items-center h-[35px] px-[15px] border border-solid rounded-[18px] border-[#EEEEEE]'>
+          검색{' '}
+          <img
+            className='ml-[6px] !h-[15px] !w-[15px]'
+            src='/logo_search.png'
+          />
+        </button>
+        <button className='h-[35px] px-[15px] border border-solid rounded-[18px] border-[#EEEEEE]'>
+          세일상품
+        </button>
+        <button className='h-[35px] px-[15px] border border-solid rounded-[18px] border-[#EEEEEE]'>
+          단독상품
+        </button>
+        <button className='h-[35px] px-[15px] border border-solid rounded-[18px] border-[#EEEEEE]'>
+          품절포함
+        </button>
+      </div>
     </div>
   );
 }
