5c00796680e9adcbbdbb4106066d291b164d4197
l9e7e <ehdgur920907@naver.com>
2023-05-05 16:50:50 +0900

    Add NoResult

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { SyntheticEvent, useEffect, useRef, useState } from 'react';
+import NoResult from './NoResult';
 
 function App() {
   const [goodsList, setGoodsList] = useState<Goods[]>([]);
@@ -167,47 +168,50 @@ function App() {
         )}
       </div>
       <div className='h-[10px] bg-[#F1F1F1]' />
-      <div className='flex flex-wrap'>
-        {goodsList.map((goods, index) => {
-          return (
-            <div key={index} className='basis-1/2' ref={goodsRef}>
-              <div className='relative'>
-                <img
-                  className='!h-[226px]'
-                  src={goods.imageUrl}
-                  onError={(e: SyntheticEvent<HTMLImageElement, Event>) => {
-                    e.currentTarget.src = '/img_default.jpeg';
-                  }}
-                />
-                {goods.isExclusive && (
-                  <span className='text-[12px] font-normal leading-[18px] -tracking-[0.5px] absolute -bottom-[12px] left-[10px] text-white bg-[#18A286] px-[6px] py-[4px]'>
-                    단독
-                  </span>
-                )}
-              </div>
-              <div className='mx-[10px] mb-[20px]'>
-                <p className='text-[11px] font-normal leading-[16px] mt-[20px]'>
-                  {goods.brandName}
-                </p>
-                <p className='mt-[8px] text-[14px] font-bold leading-[18px] line-clamp-2 break-all'>
-                  {goods.goodsName}
-                </p>
-                <div className='mt-[4px] flex justify-between'>
-                  <span className='text-[16px] font-medium leading-[24px]'>
-                    {goods.price.toLocaleString()}원
-                  </span>
-                  <span className='text-[16px] font-medium leading-[24px] text-[#FF0000]'>
-                    {goods.saleRate}%
-                  </span>
+      {goodsList.length === 0 && <NoResult />}
+      {goodsList.length > 0 && (
+        <div className='flex flex-wrap'>
+          {goodsList.map((goods, index) => {
+            return (
+              <div key={index} className='basis-1/2' ref={goodsRef}>
+                <div className='relative'>
+                  <img
+                    className='!h-[226px]'
+                    src={goods.imageUrl}
+                    onError={(e: SyntheticEvent<HTMLImageElement, Event>) => {
+                      e.currentTarget.src = '/img_default.jpeg';
+                    }}
+                  />
+                  {goods.isExclusive && (
+                    <span className='text-[12px] font-normal leading-[18px] -tracking-[0.5px] absolute -bottom-[12px] left-[10px] text-white bg-[#18A286] px-[6px] py-[4px]'>
+                      단독
+                    </span>
+                  )}
                 </div>
-                <p className='text-[11px] font-normal leading-[12px] line-through text-[#AAAAAA]'>
-                  {goods.normalPrice.toLocaleString()}
-                </p>
+                <div className='mx-[10px] mb-[20px]'>
+                  <p className='text-[11px] font-normal leading-[16px] mt-[20px]'>
+                    {goods.brandName}
+                  </p>
+                  <p className='mt-[8px] text-[14px] font-bold leading-[18px] line-clamp-2 break-all'>
+                    {goods.goodsName}
+                  </p>
+                  <div className='mt-[4px] flex justify-between'>
+                    <span className='text-[16px] font-medium leading-[24px]'>
+                      {goods.price.toLocaleString()}원
+                    </span>
+                    <span className='text-[16px] font-medium leading-[24px] text-[#FF0000]'>
+                      {goods.saleRate}%
+                    </span>
+                  </div>
+                  <p className='text-[11px] font-normal leading-[12px] line-through text-[#AAAAAA]'>
+                    {goods.normalPrice.toLocaleString()}
+                  </p>
+                </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/NoResult.tsx
@@ -0,0 +1,10 @@
+export default function NoResult() {
+  return (
+    <div className='flex flex-col items-center justify-center absolute inset-0 z-20'>
+      <img src='/img_no_result.png' className='!h-[90px] !w-[90px]' />
+      <p className='text-[14px] font-normal leading-[21px] text-[#AAAAAA]'>
+        검색 결과 없음
+      </p>
+    </div>
+  );
+}
